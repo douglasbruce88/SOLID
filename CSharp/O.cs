5c795b7b7722e056856c7998a91a0b0b945c8ba8
douglasbruce88 <douglasbruce88@gmail.com>
2016-05-22 13:36:13 +0100

    Open-Close principle

---
+++ CSharp/O.cs
@@ -0,0 +1,28 @@
+﻿
+namespace CSharp
+{
+    public abstract class ClosedForModification
+    {
+        public bool CanRollAgain (int firstDieScore, int secondDieScore)
+        {
+            return CanRollAgainImpl (firstDieScore, secondDieScore);
+        }
+
+        public abstract bool CanRollAgainImpl (int firstDieScore, int secondDieScore);
+    }
+
+    public class ThreeDoubles : ClosedForModification
+    {
+        readonly bool LastTwoRollsAreDoubles;
+        public ThreeDoubles (bool lastTwoRollsAreDoubles)
+        {
+            this.LastTwoRollsAreDoubles = lastTwoRollsAreDoubles;
+        }
+
+        public override bool CanRollAgainImpl (int firstDieScore, int secondDieScore)
+        {
+            return !LastTwoRollsAreDoubles && (firstDieScore == secondDieScore);
+        }
+    }
+}
+
---
+++ FSHarp/O.fs
@@ -0,0 +1,8 @@
+﻿namespace FSharp
+
+module O = 
+
+  let canRollAgain (firstRoll : int) (secondRoll : int) ruleSet : bool = 
+      ruleSet firstRoll secondRoll
+
+  let caller = canRollAgain 1 2
